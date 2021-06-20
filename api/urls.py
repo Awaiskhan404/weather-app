4284500be3142f07bdfa85b2d9fb98aa476664d4
Awaiskhan404 <contact@awaiskhan.com.pk>
2021-06-20 20:52:19 +0500

    updated search

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .import views
 urlpatterns = [
     path('',views.index,name="index"),
-    path('v2/<str:city>',views.GetCurrentStateByCity,name="GetCurrentStateByCity"),
+    path('city/',views.GetCurrentStateByCity,name="GetCurrentStateByCity"),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ import requests
 def index(request):
      city='lahore'
      ip=request.META.get("REMOTE_ADDR")
-     ip='111.119.187.1'
+     ip='182.186.80.37'
      CurrentState=GetCurrentState(ip=ip)
      main=CurrentState["main"]
      tempreture=round(main["temp"]-270)
@@ -15,14 +15,20 @@ def index(request):
           'tempreture':tempreture,
           'state':CurrentState,
      })
-def GetCurrentStateByCity(request,city):
+def GetCurrentStateByCity(request):
+     city = request.GET.get('city')
      appid="a2fe069ff0dc1dbc0d9bdf7a73d7ace5"
      protocol="https://" 
      url=protocol+"api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ appid
      response=requests.get(url)
      data=response.json()
-     context=GetStats(data)
-     return JsonResponse(context,safe=False)
+     main=data["main"]
+     tempreture=round(main["temp"]-270)
+     print(tempreture)
+     return render(request,'api/index.html',{
+          'tempreture':tempreture,
+          'state':data,
+     })
      
 
 
