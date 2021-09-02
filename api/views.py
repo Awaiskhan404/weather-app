5dbda9d9c639fdbd7fe8a8dcfcd77a42f7a098d0
Awais khan <awais.khan@secomind.ai>
2021-09-02 19:25:46 +0500

    new update

--- api/views.py
+++ api/views.py
@@ -10,25 +10,41 @@ def index(request):
      CurrentState=GetCurrentState(ip=ip)
      main=CurrentState["main"]
      tempreture=round(main["temp"]-270)
-     print(tempreture)
+     if tempreture>30:
+         headline='Tempreture is ' +str(tempreture)+' Its hot outside Stay inside'
+     elif tempreture in range(20,30):
+         headline='Tempreture is ' +str(tempreture)+' Its good time for outing'
+     elif tempreture in range(10,20):
+         headline='Tempreture is ' +str(tempreture)+' Its good time for outing. Take care'
+     elif tempreture in range(0,10):
+         headline='Tempreture is ' +str(tempreture)+' Its very cold. Take care'
+     print(headline)
      return render(request,'api/index.html',{
           'tempreture':tempreture,
           'state':CurrentState,
+          'headline':headline
      })
 def GetCurrentStateByCity(request):
      city = request.GET.get('city')
      appid="a2fe069ff0dc1dbc0d9bdf7a73d7ace5"
-     protocol="https://" 
+     protocol="https://"
      url=protocol+"api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ appid
      response=requests.get(url)
      data=response.json()
      main=data["main"]
      tempreture=round(main["temp"]-270)
      print(tempreture)
+     if tempreture>30:
+          headline='Tempreture is ' +str(tempreture)+' Its hot outside Stay inside'
+     elif tempreture in range(20,30):
+          headline='Tempreture is ' +str(tempreture)+' Its good time for outing'
+     elif tempreture in range(10,20):
+          headline='Tempreture is ' +str(tempreture)+' Its good time for outing. Take care'
+     elif tempreture in range(0,10):
+          headline='Tempreture is ' +str(tempreture)+' Its very cold. Take care'
+     print(headline)
      return render(request,'api/index.html',{
           'tempreture':tempreture,
           'state':data,
+          'headline':headline
      })
-     
-
-
